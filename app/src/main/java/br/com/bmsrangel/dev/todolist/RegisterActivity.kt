88a526336c857091fab6d3772b9e87a774a6ae16
bmsrangel <bmsrangel@hotmail.com>
2023-03-02 21:06:52 -0300

    Signed the app, added Firebase dependencies and created RegisterActivity and LoginActivity

---
+++ app/src/main/java/br/com/bmsrangel/dev/todolist/RegisterActivity.kt
@@ -0,0 +1,11 @@
+package br.com.bmsrangel.dev.todolist
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class RegisterActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register)
+    }
+}
