90e531e230cc7204a6b0570f91648a65b07ba235
bmsrangel <bmsrangel@hotmail.com>
2023-03-11 19:40:47 -0300

    Implemented layout for Login, Register and Main

--- app/src/main/java/br/com/bmsrangel/dev/todolist/LoginActivity.kt
+++ app/src/main/java/br/com/bmsrangel/dev/todolist/LoginActivity.kt
@@ -1,11 +1,23 @@
 package br.com.bmsrangel.dev.todolist
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        supportActionBar?.hide()
+
+        val registerButtonRef = findViewById<Button>(R.id.btnRegister)
+        registerButtonRef.setOnClickListener {
+            val intent = Intent(this, RegisterActivity::class.java)
+            startActivity(intent)
+        }
     }
+
+
 }
--- app/src/main/java/br/com/bmsrangel/dev/todolist/MainActivity.kt
+++ app/src/main/java/br/com/bmsrangel/dev/todolist/MainActivity.kt
@@ -2,10 +2,57 @@ package br.com.bmsrangel.dev.todolist
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.util.SparseBooleanArray
+import android.widget.ArrayAdapter
+import android.widget.Button
+import android.widget.EditText
+import android.widget.ListView
+import androidx.core.util.keyIterator
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val todos = arrayListOf<String>("Task 1", "Task 2", "Task 3", "Task 4")
+
+        val todosListRef = findViewById<ListView>(R.id.todosList)
+        val adapter = ArrayAdapter(this, android.R.layout.simple_list_item_multiple_choice, todos)
+        todosListRef.choiceMode = ListView.CHOICE_MODE_MULTIPLE
+        todosListRef.adapter = adapter
+
+        val editTextDescription = findViewById<EditText>(R.id.editTxtDescription)
+
+        val addButtonRef = findViewById<Button>(R.id.btnAdd)
+        val removeButtonRef = findViewById<Button>(R.id.btnRemove)
+        val clearButtonRef = findViewById<Button>(R.id.btnClear)
+
+        addButtonRef.setOnClickListener {
+            val description = editTextDescription.text.toString()
+            todos.add(description)
+            adapter.notifyDataSetChanged()
+            editTextDescription.text.clear()
+        }
+
+        removeButtonRef.setOnClickListener {
+            val selectedTodos: SparseBooleanArray = todosListRef.checkedItemPositions;
+            val todosLength = todosListRef.count
+            var cursor = todosLength - 1
+
+            while (cursor >= 0) {
+                if (selectedTodos.get(cursor)) {
+                    adapter.remove(todos[cursor])
+                }
+                cursor--
+            }
+            selectedTodos.clear()
+            adapter.notifyDataSetChanged()
+        }
+
+        clearButtonRef.setOnClickListener {
+            todos.clear()
+            adapter.notifyDataSetChanged()
+        }
     }
 }
